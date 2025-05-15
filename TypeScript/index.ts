06cf026eabcff68c11c7891ebe2ef32ea661c1f7
ujjal_roy <ujjalroy7862@gmail.com>
2025-05-15 21:48:18 +0600

    Implement sortedArrayUsingHashTable function for unique number sorting

--- TypeScript/index.ts
+++ TypeScript/index.ts
@@ -6,3 +6,25 @@ class Car {
 
 const car = new Car("Hello, TypeScript!");
 console.log(car);
+// function sortedArray(numbers: Array<number>): Array<number> {
+//   const sortedArr: number[] = [];
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] !== numbers[i + 1]) {
+//       sortedArr.push(numbers[i]);
+//     }
+//   }
+//   return sortedArr;
+// }
+
+function sortedArrayUsingHashTable(numbers: Array<number>): Array<number> {
+  const hashTable: { [key: number]: boolean } = {};
+  const sortedArr: number[] = [];
+  for (let i = 0; i < numbers.length; i++) {
+    if (!hashTable[numbers[i]]) {
+      hashTable[numbers[i]] = true;
+      sortedArr.push(numbers[i]);
+    }
+  }
+  return sortedArr;
+}
+console.log(sortedArrayUsingHashTable([1, 2, 3, 3, 3, 4, 5, 6, 7, 5]));
