7b020c401aed9a41ad2fc94377f453982584d567
ujjal_roy <ujjalroy7862@gmail.com>
2025-05-17 22:38:13 +0600

    Add Collection interface and List class implementation for managing collections

--- TypeScript/interfact.js
+++ TypeScript/interfact.js
@@ -9,3 +9,27 @@ var c = {
 };
 console.log(c);
 console.log(c.run());
+var List = /** @class */ (function () {
+    function List() {
+        this.items = [];
+    }
+    List.prototype.add = function (t) {
+        this.items.push(t);
+    };
+    List.prototype.remove = function (t) {
+        var index = this.items.indexOf(t);
+        if (index !== -1) {
+            this.items.splice(index, 1);
+        }
+    };
+    List.prototype.asArray = function () {
+        return this.items;
+    };
+    return List;
+}());
+var list = new List();
+list.add(1);
+list.add(2);
+list.add(3);
+list.remove(2);
+console.log(list.asArray()); // [1, 3]
--- TypeScript/interfact.ts
+++ TypeScript/interfact.ts
@@ -21,3 +21,35 @@ let c: Command<number, string> = {
 };
 console.log(c);
 console.log(c.run());
+
+interface Collection<T> {
+  add(t: T): void;
+  remove(t: T): void;
+  asArray(): T[];
+}
+
+class List<T> implements Collection<T> {
+  private items: T[] = [];
+
+  add(t: T): void {
+    this.items.push(t);
+  }
+
+  remove(t: T): void {
+    const index = this.items.indexOf(t);
+    if (index !== -1) {
+      this.items.splice(index, 1);
+    }
+  }
+
+  asArray(): T[] {
+    return this.items;
+  }
+}
+
+const list = new List<number>();
+list.add(1);
+list.add(2);
+list.add(3);
+list.remove(2);
+console.log(list.asArray()); // [1, 3]
